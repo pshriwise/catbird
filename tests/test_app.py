f7e3848091f0af697746821e30d578d2e7a1704e
Patrick Shriwise <pshriwise@gmail.com>
2022-05-16 18:39:26 -0500

    Adding tests for a handful of OpenMCProble attributes

---
+++ tests/test_app.py
@@ -0,0 +1,22 @@
+
+import pytest
+
+from catbird import Catbird
+
+def test_app():
+
+    app = Catbird.from_json('openmc.json')
+
+    omc = app['OpenMCCellAverageProblem']
+
+    # test type-checking
+    with pytest.raises(ValueError) as e:
+        omc.batches = '100'
+
+    with pytest.raises(ValueError) as e:
+        omc.initial_properties = 'xmll'
+
+    omc.initial_properties = 'xml'
+
+    assert omc.default_ghosting == False
+    assert omc.k_trigger == None
