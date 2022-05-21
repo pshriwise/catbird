4bcfb6d9b0668245a837acf3f7c298689ab3c924
Patrick Shriwise <pshriwise@gmail.com>
2022-05-21 12:44:41 -0500

    Adding some more setup info

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ kwargs = {
     'packages': find_packages(exclude=['tests*']),
     'author': 'Patrick Shriwise',
     'author_email': 'pshriwise@gmail.com',
+    'license' : 'MIT',
+    'keywords': ['MOOSE'],
     'download_url': 'https://github.com/pshriwise/catbird',
+    'url': 'https://github.com/pshriwise/catbird',
     'python_requires': '>=3.6',
     'install_requires': ['numpy'],
     'extras_require': {
