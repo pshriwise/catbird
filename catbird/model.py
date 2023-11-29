7de02d1c9aa21af652a78b64e1820b14115fccab
Helen Brooks <helen.brooks@ukaea.uk>
2023-11-29 11:57:21 +0000

    do not use a dictionary in moose model

--- catbird/model.py
+++ catbird/model.py
@@ -5,13 +5,17 @@ class MooseModel():
         if params==None:
             params=self.default_params()
 
-        self.objects={}
+        self.moose_objects=[]
 
         for category, cat_type in params.items():
             cat_dict=parse_blocks_types(json_obj,category,category_names=[cat_type])
             cat_constructor=cat_dict[cat_type]
             cat_instance=cat_constructor()
-            self.objects[category]=cat_instance
+
+            # Prefer non-capitalised attributes
+            attr_name=category.lower()
+            setattr(self, attr_name, cat_instance)
+            self.moose_objects.append(attr_name)
 
     @staticmethod
     def default_params():
@@ -24,6 +28,7 @@ class MooseModel():
 
     def to_str(self):
         model_str=""
-        for name, obj in self.objects.items():
+        for obj_type in self.moose_objects:
+            obj=getattr(self,obj_type)
             model_str+=obj.to_str()
         return model_str
